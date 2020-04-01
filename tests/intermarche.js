75b36448271415ad60a7c1afea17cee870978987
Baptiste Bouffaut <baptiste.bouffaut@gmail.com>
2020-04-02 00:29:29 +0200

    implement puppeteer-extra to try to bypass catcha

--- tests/intermarche.js
+++ tests/intermarche.js
@@ -1,15 +1,38 @@
 /**
- * @name Amazon product search
+ * @name Intermarche look for new drive slots
  * @desc Searches Amazon.com for a product and checks if the results show up
  */
 
 const assert = require('assert')
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer-extra')
+
+
+// add stealth plugin and use defaults (all evasion techniques)
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')
+puppeteer.use(StealthPlugin())
+
+const args = [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+        '--disable-infobars',
+        '--window-position=0,0',
+        '--ignore-certifcate-errors',
+        '--ignore-certifcate-errors-spki-list',
+        '--user-agent="Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3312.0 Safari/537.36"'
+    ];
+
+    const options = {
+        args,
+        headless: true,
+        ignoreHTTPSErrors: true,
+        userDataDir: './tmp'
+    };
+
 let browser
 let page
 
 before(async () => {
-  browser = await puppeteer.launch()
+  browser = await puppeteer.launch(options)
   page = await browser.newPage()
 })
 
@@ -19,7 +42,7 @@ describe('Intermarche HomePage', () => {
     await page.goto('https://intermarche.com',{ waitUntil: 'networkidle0' })
     //const seConnecterButton = await page.$('.connect-prehome')
     //assert.ok(seConnecterButton)
-    await page.screenshot({ path: 'amz.png'})
+    await page.screenshot({ path: 'intermarche.png'})
   }).timeout(20000)
 
   //it('shows search results after search input', async () => {
